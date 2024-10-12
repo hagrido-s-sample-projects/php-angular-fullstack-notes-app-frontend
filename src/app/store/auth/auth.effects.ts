b0b3cc651ec0f02dba06a4764dfe09000478fdfe
hagrid862 <n.okroj@icloud.com>
2024-10-12 15:25:39 +0200

    refactor: remove auth actions, effects, reducer, and service files

--- src/app/store/auth/auth.effects.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { of } from "rxjs";
-import { catchError, map, mergeMap } from "rxjs/operators";
-
-import * as AuthActions from './auth.actions';
-import { AuthService } from "../../services/auth.service";
