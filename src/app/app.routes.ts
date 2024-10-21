f498057f5a72d180871180a0896250ed33ea31f2
hagrid862 <n.okroj@icloud.com>
2024-10-21 10:12:43 +0200

    chore: added dashboard component to app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
 export const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
