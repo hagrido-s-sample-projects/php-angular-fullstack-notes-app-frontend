4f92d868c24d2a20bc4d7944e9e6835cb427b3d0
hagrid862 <n.okroj@icloud.com>
2024-10-21 09:45:23 +0200

    feat: implement registration logic with NgRx store and error handling

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../store/auth/auth.actions';
+import { selectAuthError } from '../store/auth/auth.selectors';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -12,24 +14,37 @@ import { NgIf } from '@angular/common';
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy {
   username: string = '';
   email: string = '';
   password: string = '';
 
   errorMessage: string | null = null;
+  private errorSubscription: Subscription | null = null;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.errorSubscription = this.store.select(selectAuthError).subscribe(error => {
+      if (error) {
+        this.errorMessage = error.message || 'Something went wrong';
+      } else {
+        this.errorMessage = null;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.errorSubscription) {
+      this.errorSubscription.unsubscribe();
+    }
+  }
 
   register() {
-    this.authService.register(this.username, this.email, this.password).subscribe(
-      res => {
-        if (res.status === 'SUCCESS') {
-          this.router.navigate(['/login']);
-        } else {
-          this.errorMessage = res.error || 'Something went wrong';
-        }
-      }
-    );
+    if (this.username && this.email && this.password) {
+      this.store.dispatch(AuthActions.register({ username: this.username, email: this.email, password: this.password }));
+    } else {
+      this.errorMessage = 'All fields are required';
+    }
   }
 }
