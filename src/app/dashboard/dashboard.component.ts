deca051bbce4d779cd0adceb960b557ad61faeec
hagrid862 <n.okroj@icloud.com>
2024-10-21 11:31:01 +0200

    feat: created good looking menu in header of dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgClass } from '@angular/common';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-
+  isMenuOpen = false;
 }
