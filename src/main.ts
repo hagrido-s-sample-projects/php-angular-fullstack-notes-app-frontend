133e86e40f7a5503f99c88efd10380606d84eb19
hagrid862 <n.okroj@icloud.com>
2024-10-21 10:53:00 +0200

    feat: enabled support for redux devtools

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,19 @@ import { AuthEffects } from './app/store/auth/auth.effects';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { environment } from './enviroment/enviroment';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideStore({ auth: authReducer }),
     provideEffects([AuthEffects]),
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
 }).catch(err => {
   console.error('Error during bootstrap:', err);
