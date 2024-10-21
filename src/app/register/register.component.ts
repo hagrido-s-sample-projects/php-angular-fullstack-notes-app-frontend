afe59dd6315663770991f5ba7719569362f9e1b7
hagrid862 <n.okroj@icloud.com>
2024-10-21 08:52:47 +0200

    feat: add error message display in registration form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,11 +3,12 @@ import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, NgIf],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
