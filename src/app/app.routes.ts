58181ba892a3258ebae7a876131643fbdb25a700
hagrid862 <n.okroj@icloud.com>
2024-10-21 10:39:57 +0200

    feat: added guards to dashboard and other pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './guards/auth.guard';
+import { NonAuthGuard } from './guards/non-auth.guard';
+
 export const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: '', component: HomepageComponent, canActivate: [NonAuthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [NonAuthGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [NonAuthGuard] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
