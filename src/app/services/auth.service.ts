ad31be1f20ea91df04defb53157c354b17fc3d90
hagrid862 <n.okroj@icloud.com>
2024-10-21 09:05:40 +0200

    chore: removed unused enviroment.ts file and import of it inside auth.service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, Observable, of } from 'rxjs';
-import { environment } from '../../enviroment/enviroment';
 
 @Injectable({
   providedIn: 'root',
