ed39ce15bec2d6f1f24950e5bdb34c87c97d1063
hagrid862 <n.okroj@icloud.com>
2024-10-21 09:58:04 +0200

    chore: updated form component and added binding fields value to variables

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,20 +2,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AuthService } from '../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, FormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   title = 'Login';
 
+  username: string = '';
+  password: string = '';
+  errorMessage: string | null = null;
+
   constructor(private store: Store, private authService: AuthService, private router: Router) {}
 
-  onLogin(username: string, password: string) {
+  login() {
     
   }
 }
