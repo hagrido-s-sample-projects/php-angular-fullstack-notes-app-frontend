0b95645ade220aa78831e27bff089f8cecf65282
hagrid862 <n.okroj@icloud.com>
2024-10-23 11:15:34 +0200

    feat: styled dialog to looks better and added close button

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,17 +1,28 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   isCreateDialogOpen = false;
+  noteTitle: string = '';
+  errorMessage: string | null = null;
 
   showCreateNoteDialog() {
     this.isCreateDialogOpen = true;
+  }
+
+  closeCreateDialog() {
+    this.isCreateDialogOpen = false;
+  }
+
+  createNote() {
+    // Implement note creation logic here
   }
 }
