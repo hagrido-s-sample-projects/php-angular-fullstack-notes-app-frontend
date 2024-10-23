f384342493af20a9c0dde52a81991311197c0bd4
hagrid862 <n.okroj@icloud.com>
2024-10-23 12:02:11 +0200

    chore: added note store to app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,14 +10,16 @@ import { routes } from './app.routes';
 import { authReducer } from './store/auth/auth.reducer';
 import { AuthEffects } from './store/auth/auth.effects';
 import { AuthService } from './services/auth.service';
+import { noteReducer } from './store/note/note.reducer';
+import { NoteEffects } from './store/note/note.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideStore({ auth: authReducer }),
-    provideEffects([AuthEffects]),
+    provideStore({ auth: authReducer, note: noteReducer }),
+    provideEffects([AuthEffects, NoteEffects]),
     provideStoreDevtools({ maxAge: 25, logOnly: true }),
     provideHttpClient(withFetch()),
     AuthService,
