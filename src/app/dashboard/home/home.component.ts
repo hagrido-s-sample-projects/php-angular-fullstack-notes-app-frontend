62c994cc8f30e3fd93e88a23bb2a24f4bb41131a
hagrid862 <n.okroj@icloud.com>
2024-10-23 11:06:50 +0200

    feat: added dialog to create note

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isCreateDialogOpen = false;
 
+  showCreateNoteDialog() {
+    this.isCreateDialogOpen = true;
+  }
 }
